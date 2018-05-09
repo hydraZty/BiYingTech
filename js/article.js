0e598ab99c142bd53b0aaa5cb080e7d5cc8ad34a
hydraZhang <670688667@qq.com>
2018-05-09 11:49:30 +0800

    三个语言版本完成

---
+++ js/article.js
@@ -0,0 +1,122 @@
+/**
+ * Created by zhangtianyi on 2018/5/2.
+ */
+
+
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+
+
+
+
+var count=0;
+
+
+var $articles;
+$.get("../js/article.xml",function(xml){
+    //xml即为可以读取使用的内容，具体读取见第2点
+    var $xml=$(xml);
+    $articles=$xml.find("article");
+
+
+    num=getQueryString("num")-1;
+    console.log(num);
+    if(num>=0){
+        var article=$articles[num];
+        var title=article.getElementsByTagName("title")[0].innerHTML;
+        $("#title").text(title);
+
+        var date=article.getElementsByTagName("date")[0].innerHTML;
+        $("#date").text(date);
+
+
+        var banner=article.getElementsByTagName("banner")[0].innerHTML;
+        $("#banner").attr("src",banner);
+
+
+        var section=article.getElementsByTagName("section")[0];
+        $("#content").html(section);
+
+
+
+        var sign1=article.getElementsByTagName("sign1")[0].innerHTML;
+        $("#sign-1").text(sign1);
+
+
+        var sign2=article.getElementsByTagName("sign2")[0].innerHTML;
+        $("#sign-2").text(sign2);
+
+    }else{
+        for(var i=count;i<4;i++){
+            var article=$articles[i];
+            var title=article.getElementsByTagName("title")[0].innerHTML;
+            var date=article.getElementsByTagName("date")[0].innerHTML;
+            var thumbnail=article.getElementsByTagName("thumbnail")[0].innerHTML;
+            var section=article.getElementsByTagName("section")[0].innerHTML.slice(0,300);
+            var num=i+1+"";
+            var ele=$('<a class="media" href="article.html?num='+num+'"> <div class="media-left"> <img class="video-icon" src="../image/picture-icon.png" alt=""> <img src="'+thumbnail+'" alt="icon"> </div> <div class="media-body"> <b class="media-heading">'+title+'</b> <p class="date">'+date+'</p> <p class="content clamp4">'+section+'</p></div></a>');
+
+            $(".index-news-wrapper").append(ele);
+
+            $(".nav-picture").append(ele);
+            count++;
+
+        }
+
+
+
+    }
+
+
+    var clamp4=document.getElementsByClassName("clamp4");
+    try{
+        for(var i=0;i<=clamp4.length;i++){
+            $clamp(clamp4[i],{clamp:4,useNativeClamp: false})
+        }
+    }catch(err){
+        console.log(err)
+    }
+
+});
+
+
+$("#view-more").click(function(){
+
+
+    console.log("click");
+    for(var i=count;i<(count+4)&&i<$articles.length;i++){
+        var article=$articles[i];
+        var title=article.getElementsByTagName("title")[0].innerHTML;
+        var date=article.getElementsByTagName("date")[0].innerHTML;
+        var thumbnail=article.getElementsByTagName("thumbnail")[0].innerHTML;
+        var section=article.getElementsByTagName("section")[0].innerHTML.slice(0,300);
+        var num=i+1+"";
+        var ele=$('<a class="media" href="article.html?num='+num+'"> <div class="media-left"> <img class="video-icon" src="../image/picture-icon.png" alt=""> <img src="'+thumbnail+'" alt="icon"> </div> <div class="media-body"> <b class="media-heading">'+title+'</b> <p class="date">'+date+'</p> <p class="content clamp4">'+section+'</p></div></a>');
+
+        $(".index-news-wrapper").append(ele);
+
+        $(".nav-picture").append(ele);
+        count++;
+
+        if(count==$articles.length){
+            $("#view-more").hide();
+            $(".noting-more").show();
+        }
+    }
+
+    var clamp4=document.getElementsByClassName("clamp4");
+    try{
+        for(var i=0;i<=clamp4.length;i++){
+            $clamp(clamp4[i],{clamp:4,useNativeClamp: false})
+        }
+    }catch(err){
+        console.log(err)
+    }
+})
+
--- js/myJs.js
+++ js/myJs.js
@@ -3,6 +3,8 @@
  */
 
 
+
+
 new navScroll({
     nav : {                  //导航跳转功能，不需要可以删除此代码块
         id : 'navigation',   //点击跳转到容器的导航ID 请把 ID 设置给 UL
@@ -14,13 +16,79 @@ new navScroll({
 
 
 // var clamp2=document.getElementsByClassName("clamp2");
-var clamp3=document.getElementsByClassName("clamp3");
 
-for(var i=0;i<=clamp3.length;i++){
-    $clamp(clamp3[i],{clamp:3,useNativeClamp: false})
+
+
+
+
+var clamp2=document.getElementsByClassName("clamp2");
+try{
+    for(var i=0;i<=clamp2.length;i++){
+        $clamp(clamp2[i],{clamp:2,useNativeClamp: false})
+    }
+
+}catch (err){
+    console.log(err);
+
+}
+
+
+var clamp3=document.getElementsByClassName("clamp3");
+try{
+    for(var i=0;i<=clamp3.length;i++){
+        $clamp(clamp3[i],{clamp:3,useNativeClamp: false})
+    }
+}catch (err){
+    console.log(err)
 }
 
 
 
-// $clamp(clamp2,{clamp:2,useNativeClamp: false});
-// $clamp(clamp3,{clamp:3,useNativeClamp: false});
+
+
+
+var clamp4=document.getElementsByClassName("clamp4");
+try{
+    for(var i=0;i<=clamp4.length;i++){
+        $clamp(clamp4[i],{clamp:4,useNativeClamp: false})
+    }
+}catch(err){
+    console.log(err)
+}
+
+
+$("#tab-picture").click(function(){
+    $(this).addClass("active")
+    $("#tab-video").removeClass("active");
+    $(".nav-picture").show();
+    $(".nav-video").hide();
+
+})
+
+
+$("#tab-video").click(function(){
+
+    $(this).addClass("active")
+    $("#tab-picture").removeClass("active");
+
+    $(".nav-video").show();
+    $(".nav-picture").hide();
+
+    var clamp4=document.getElementsByClassName("clamp4");
+    try{
+        for(var i=0;i<=clamp4.length;i++){
+            $clamp(clamp4[i],{clamp:4,useNativeClamp: false})
+        }
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
+$(".recruitment-title").click(function(){
+    console.log($(this).next().css("display"));
+    if($(this).next().css("display")=="none"){
+        $(".recruitment-title").next().slideUp();
+        $(this).next().slideDown();
+    }
+})
